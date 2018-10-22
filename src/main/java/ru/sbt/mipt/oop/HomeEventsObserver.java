dad6f8cdbeb0220662a17eed95cb365a138b9408
Roman Orekhov <orekhovrv@gmail.com>
2018-10-22 16:24:20 +0300

    New Class HomeEventsObserver. It manages event processing.

--- src/main/java/ru/sbt/mipt/oop/HomeEventsObserver.java
+++ src/main/java/ru/sbt/mipt/oop/HomeEventsObserver.java
@@ -1,4 +1,26 @@
 package ru.sbt.mipt.oop;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class HomeEventsObserver {
+    private Collection<EventProcessor> configureEventProcessors() {
+        Collection<EventProcessor> eventProcessors = new ArrayList<>();
+        eventProcessors.add(new LightsEventProcessor());
+        eventProcessors.add(new DoorEventProcessor());
+        eventProcessors.add(new HallDoorEventProcessor());
+        return eventProcessors;
+    }
+
+    public void runEventsCycle(SmartHome smartHome) {
+        SensorEvent event = RandomSensorEventProvider.getNextSensorEvent();
+        Collection<EventProcessor> eventProcessors = this.configureEventProcessors();
+        while (event != null) {
+            System.out.println("Got event: " + event);
+            for (EventProcessor eventProcessor : eventProcessors) {
+                eventProcessor.processEvent(smartHome, event);
+            }
+            event = RandomSensorEventProvider.getNextSensorEvent();
+        }
+    }
 }
